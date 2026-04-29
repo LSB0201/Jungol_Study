1f8d31b5e4d7806b38e357dd95d5fe7713ad8dc4
olyme <olyme111@naver.com>
2026-04-29 09:56:56 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.cpp
@@ -0,0 +1,33 @@
+#include <string> // 분수의 덧셈
+#include <vector>
+
+using namespace std;
+
+int gcd(int a, int b) // 최대공약수 구하기
+{
+    while (b != 0)
+    {
+        int temp = a % b;
+        a = b;
+        b = temp;
+    }
+    return a;
+}
+
+vector<int> solution(int numer1, int denom1, int numer2, int denom2) {
+
+    int sum_nu = 0, de = 0;
+    int result_gcd = 0;
+
+    numer1 *= denom2;
+    numer2 *= denom1;
+    de = denom1 * denom2;
+    
+    sum_nu = numer1 + numer2;
+
+    result_gcd = gcd(sum_nu, de);
+
+    vector<int> answer = { sum_nu / result_gcd, de / result_gcd };
+
+    return answer;
+}
