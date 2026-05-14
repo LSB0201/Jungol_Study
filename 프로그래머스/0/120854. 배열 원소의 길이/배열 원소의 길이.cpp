807b30c0d988337f3a8f578eb6e1e7fc44690bb7
olyme <olyme111@naver.com>
2026-05-14 23:59:07 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.01 ms, Memory: 4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<string> strlist) {
+    vector<int> answer(strlist.size());
+    int i = 0;
+    
+    for (const string &str : strlist)
+    {
+        answer[i] = str.length();
+        ++i;
+    }
+    return answer;
+}
