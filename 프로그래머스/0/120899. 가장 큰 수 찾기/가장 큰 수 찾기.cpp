173de00556016c7096bdef8f521fe3ed87e11ffa
olyme <olyme111@naver.com>
2026-05-18 09:20:51 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.cpp
@@ -18,6 +18,6 @@ vector<int> solution(vector<int> array) {
     return answer;
 }
 
-// 밑의 함수를 사용하면 더 O(N)으로 해결 가능
+// 밑의 함수를 사용하면 O(N)으로 해결 가능
 // <algorithm>, max_element(시작, 끝): 범위 내에서 가장 큰 값의 위치(반복자)를 반환
 // <iterator>, distance(시작_반복자, 끝_반복자): 두 반복자(Iterator) 사이에 몇 개의 요소가 있는지(거리)를 계산
