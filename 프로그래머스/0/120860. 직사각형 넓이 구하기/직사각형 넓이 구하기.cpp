6d4f355a1b154b44f30becdcd2fbc6f186be1a7e
olyme <olyme111@naver.com>
2026-05-21 00:16:34 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.cpp
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> dots) {
+    int w = 0, h = 0;
+    
+    for (int i = 1; i < 3; ++i)
+    {
+        w = std::max(w, abs(dots[i][0] - dots[0][0]));
+        h = std::max(h, abs(dots[i][1] - dots[0][1]));
+    }
+                    
+    
+    
+    
+    return w * h;
+}
