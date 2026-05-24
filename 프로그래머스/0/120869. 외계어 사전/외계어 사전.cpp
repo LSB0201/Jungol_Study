cc274f62cf7bbc46802ea96e4a02221cb77f7a54
olyme <olyme111@naver.com>
2026-05-24 14:24:18 +0900

    [level 0] Title: 외계어 사전, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.cpp
@@ -0,0 +1,32 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<string> spell, vector<string> dic) {
+    int answer = 0;
+    
+    for (string s_dic : dic)
+    {
+        bool flag = true;
+        
+        for (string s_sp : spell)
+        {
+            if (s_dic.find(s_sp) != string::npos)
+                s_dic.erase(remove(s_dic.begin(), s_dic.end(), s_sp[0]), s_dic.end());
+            else
+            {
+                flag = false;
+                break;
+            }
+        }
+        
+        if (flag == true && s_dic.empty())
+        {
+            return 1;
+        }
+    }
+    
+    return 2;
+}
